808c39b07c72fcb6c566ac4d1e8c43fcafedca73
Hussein Yehya <hussein.ali@becommerce.com.br>
2020-01-16 19:26:01 -0300

    pom

--- src/main/java/br/com/betalk/jenkins/JenkinsApplication.java
+++ src/main/java/br/com/betalk/jenkins/JenkinsApplication.java
@@ -2,12 +2,13 @@ package br.com.betalk.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsApplication {
+public class JenkinsApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(JenkinsApplication.class, args);
+//	}
 
 }
